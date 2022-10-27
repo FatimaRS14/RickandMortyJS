8a4a00eeebe0f3c470d3573e43469e80613993d3
Fatty Rojas <faty.aome.rojas14@gmail.com>
2022-10-27 11:23:32 -0500

    estilos y demas

--- app.js
+++ app.js
@@ -1,72 +1,81 @@
-const urlBase = 'https://rickandmortyapi.com/api/character/';
+const urlBase = "https://rickandmortyapi.com/api/character/";
 
-const loadData = (url, page = 1) => {
-    url += `?page=${page}`
-    fetch(url)
-    .then(respuesta => respuesta.json())
-    .then(respJson => {
-        const info = respJson.info;
-        const personajes = respJson.results;
-        if(!info.prev){
-            document.querySelector('#prev').classList.add('disabled')
-        } else {
-            document.querySelector('#prev').classList.remove('disabled')
-            document.querySelector('#prev').setAttribute('data-id', Number(page) - 1)
-        }
-        if(!info.next){
-            document.querySelector('#next').classList.add('disabled')
-        } else {
-            document.querySelector('#next').classList.remove('disabled')
-            document.querySelector('#next').setAttribute('data-id', Number(page) + 1)
-        }
-        console.log(personajes);
-        showCharacters(personajes);
+const loadData = (urlBase, page = 1) => {
+  const url = `${urlBase}?page=${page}`;
+  fetch(url)
+    .then((respuesta) => {
+      return respuesta.json();
     })
-}
-
-const navegacion = (e) => {
-    e.preventDefault();
-    if(e.target.classList.contains('btn')){
-        let page = e.target.getAttribute('data-id');
-        loadData(urlBase, page);
-    }
-}
-
-const loadInfo = e =>{
-    e.preventDefault();
-    if(e.target.classList.contains('btn')){
-        console.log(Hi);
-    }
-}
-
-loadData(urlBase);
-
-document.querySelector('#botones').addEventListener('click', navegacion);
-document.querySelector('#chartacters').addEventListener('click', loadInfo);
-
+    .then((respJson) => {
+      const info = respJson.info;
+      const personajes = respJson.results;
+      console.log(info);
+      //Validar botones
+      const btnPrev = document.querySelector('#prev');
+      const btnNext = document.querySelector('#next');
+      if(!info.prev){
+        btnPrev.classList.add('disabled');
+      } else {
+        btnPrev.classList.remove('disabled');
+        btnPrev.setAttribute('data-id', Number(page) - 1);
+      }
+      if(!info.next){
+        btnNext.classList.add('disabled');
+      } else {
+        btnNext.classList.remove('disabled');
+        btnNext.setAttribute('data-id', Number(page) + 1);
+      }
+      showCharacters(personajes);
+    });
+};
 const showCharacters = (personajes) => {
-    const contenedorRespuesta = document.querySelector('#respuesta');
-    while(contenedorRespuesta.firstChild){
-        contenedorRespuesta.removeChild(contenedorRespuesta.firstChild);
-    }
-    personajes.forEach(personaje => {
-        contenedorRespuesta.appendChild(creaCard(personaje));
-    })
-}
+  const listaPersonajes = document.querySelector('#characters');
+  while (listaPersonajes.firstChild){
+  listaPersonajes.removeChild(listaPersonajes.firstChild);
+  }
+  personajes.forEach((personaje) => {
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.classList.add('m-2');
+    div.innerHTML = creaCard(personaje);
+    listaPersonajes.appendChild(div);
+  });
+};
 
 const creaCard = (personaje) => {
-    const card = document.createElement('div');
-    card.style = 'float: left;';
-    const html = `
-    <div class="card m-2 bg-dark text-light border" style="width: 18rem; ">
-        <img src="${personaje.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${personaje.name}</h5>
-        <p class="card-text">${personaje.status}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>`;
-    card.innerHTML = html;
-  return card;
+  const html = `
+  <div class="card bg-dark text-light border" style="width: 18rem;">
+  <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
+  <div class="card-body">
+    <h5 class="card-title">${personaje.name}</h5>
+    <p class="card-text">${personaje.status}</p>
+    <a href="#" 
+    class="btn btn-primary"
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal"
+    data-id="${personaje.id}">Ver más</p>
+  </div>
+</div>
+`;
+return html;
 }
 
+const navegacion = (e)  => {
+  e.preventDefault();
+  if(e.target.classList.contains('btn')){
+    const id = e.target.getAttribute('data-id');
+    loadData(urlBase, id);
+  }
+}
+
+const loadInfo = (e) =>{
+  e.preventDefault();
+  if(e.target.classList.contains('btn')){
+    console.log('Hi!');
+  }
+}
+
+document.querySelector('#botones').addEventListener('click', navegacion);
+document.querySelector('#characters').addEventListener('click', loadInfo);
+
+loadData(urlBase);
